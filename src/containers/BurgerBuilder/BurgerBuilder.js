f2d1211e32730af1bd3e6d6133b98d0bdedfd168
ANS <36783010+answebdev@users.noreply.github.com>
2020-12-23 13:18:19 -0800

    started BurgerIngredient component

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import Aux from '../../hoc/Auxiliary';
 
 class BurgerBuilder extends Component {
-  // Render is a lifecylce method:
+  // Render is a lifecycle method:
   render() {
     return (
       <Aux>
