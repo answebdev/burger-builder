ce03ca91d06714b79436deaac1b8864da2f7c81f
ANS <36783010+answebdev@users.noreply.github.com>
2020-12-23 11:06:24 -0800

    created initial folder structure

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import Aux from '../../hoc/Auxiliary';
+
+const layout = (props) => (
+  // Auxiliary higher-order component to wrap and immediately output this component.
+  // That way, we can have adjacent elements here (<div> and <main>).
+  // This will be in the 'hoc' folder in the 'Auxiliary.js' file.
+  // And we need to import it here in this file with a capital "A" --> import Aux from '../../hoc/Auxiliary'
+  // Then we can wrap everything here with this <Aux>
+  <Aux>
+    {/* Use 'main': We want to output the component we wrap with this 'layout', so we need to use 'props.children'.
+    This allows us to use this 'layout' component as a wrapper around the core content component we want to render
+    to the screen. */}
+    <div>Toolbar, SideDrawer, Backdrop</div>
+
+    <main>{props.children}</main>
+  </Aux>
+);
+
+export default layout;
