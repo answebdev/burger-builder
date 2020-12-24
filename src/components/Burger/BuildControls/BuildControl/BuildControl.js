703a18cde3934292846738fe5308a92472d3f7b9
ANS <36783010+answebdev@users.noreply.github.com>
2020-12-24 15:07:23 -0800

    added Less/More buttons

--- src/components/Burger/BuildControls/BuildControl/BuildControl.js
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -1,13 +1,11 @@
 import React from 'react';
-import classes from './buildControl.module.css';
+import classes from './BuildControl.module.css';
 
 const buildControl = (props) => (
-  <div className={classes.buildControl}>
-    <div className={classes.Label}>
-      {props.label}
-      <button className={classes.Less}>Less</button>
-      <button className={classes.More}>More</button>
-    </div>
+  <div className={classes.BuildControl}>
+    <div className={classes.Label}>{props.label}</div>
+    <button className={classes.Less}>Less</button>
+    <button className={classes.More}>More</button>
   </div>
 );
 
--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from 'react';
+import classes from './BuildControls.module.css';
+import BuildControl from './BuildControl/BuildControl';
+
+// Array with which to loop through and build all our build controls and render them
+// (of course, we could also just hard code all the controls, but having this array is more convenient):
+const controls = [
+  { label: 'Salad', type: 'salad' },
+  { label: 'Bacon', type: 'bacon' },
+  { label: 'Cheese', type: 'cheese' },
+  { label: 'Meat', type: 'meat' },
+];
 
 const buildControls = (props) => (
-    <div>
-        
-    </div>
+  <div className={classes.BuildControls}>
+    {/* Loop through all the controls and render a build control for each of them,
+    using the constant ('controls') created above.
+    Remember too since we're using MAP, we need a key */}
+    {controls.map((ctrl) => (
+      <BuildControl key={ctrl.label} label={ctrl.label} />
+    ))}
+  </div>
 );
 
-export default buildControls
+export default buildControls;
