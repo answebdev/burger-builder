e5b6bf142000b7fb30cb996f79c7a24a68c00adb
ANS <36783010+answebdev@users.noreply.github.com>
2020-12-27 13:54:08 -0800

    reorganized folder structure

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -3,7 +3,7 @@ import Logo from '../../Logo/Logo';
 import NavigationItems from '../../Navigation/Toolbar/NavigationItems/NavigationItems';
 import classes from './SideDrawer.module.css';
 import Backdrop from '../../UI/Backdrop/Backdrop';
-import Aux from '../../../hoc/Auxiliary';
+import Aux from '../../../hoc/Auxiliary/Auxiliary';
 
 // This side drawer component will only be visible on small size mobile screens.
 const sideDrawer = (props) => {
