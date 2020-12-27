e46bf7c9ad22ad408ecbfb85f8231c68bcb5678c
ANS <36783010+answebdev@users.noreply.github.com>
2020-12-27 10:02:44 -0800

    added backdrop to side drawer

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,24 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Aux from '../../hoc/Auxiliary';
 import classes from './Layout.module.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
-const layout = (props) => (
-  // Auxiliary higher-order component to wrap and immediately output this component.
-  // That way, we can have adjacent elements here (<div> and <main>).
-  // This will be in the 'hoc' folder in the 'Auxiliary.js' file.
-  // And we need to import it here in this file with a capital "A" --> import Aux from '../../hoc/Auxiliary'
-  // Then we can wrap everything here with this <Aux>
-  <Aux>
-    {/* Use 'main': We want to output the component we wrap with this 'layout', so we need to use 'props.children'.
-    This allows us to use this 'layout' component as a wrapper around the core content component we want to render
-    to the screen. */}
-    {/* <div>Toolbar, SideDrawer, Backdrop</div> */}
-    <Toolbar />
-    <SideDrawer />
-    <main className={classes.Content}>{props.children}</main>
-  </Aux>
-);
+class Layout extends Component {
+  state = {
+    showSideDrawer: true,
+  };
 
-export default layout;
+  sideDrawerClosedHandler = () => {
+    this.setState({ showSideDrawer: false });
+  };
+
+  render() {
+    return (
+      // Auxiliary higher-order component to wrap and immediately output this component.
+      // That way, we can have adjacent elements here (<div> and <main>).
+      // This will be in the 'hoc' folder in the 'Auxiliary.js' file.
+      // And we need to import it here in this file with a capital "A" --> import Aux from '../../hoc/Auxiliary'
+      // Then we can wrap everything here with this <Aux>
+      <Aux>
+        {/* Use 'main': We want to output the component we wrap with this 'layout', so we need to use 'props.children'.
+        This allows us to use this 'layout' component as a wrapper around the core content component we want to render
+        to the screen. */}
+        {/* <div>Toolbar, SideDrawer, Backdrop</div> */}
+        <Toolbar />
+        <SideDrawer
+          open={this.state.showSideDrawer}
+          closed={this.sideDrawerClosedHandler}
+        />
+        <main className={classes.Content}>{this.props.children}</main>
+      </Aux>
+    );
+  }
+}
+
+export default Layout;
--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -2,18 +2,30 @@ import React from 'react';
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../../Navigation/Toolbar/NavigationItems/NavigationItems';
 import classes from './SideDrawer.module.css';
+import Backdrop from '../../UI/Backdrop/Backdrop';
+import Aux from '../../../hoc/Auxiliary';
 
 // This side drawer component will only be visible on small size mobile screens.
 const sideDrawer = (props) => {
+  let attachedClasses = [classes.SideDrawer, classes.Close];
+  if (props.open) {
+    attachedClasses = [classes.SideDrawer, classes.Open];
+  }
+
   return (
-    <div className={classes.SideDrawer}>
-      <div className={classes.Logo}>
-        <Logo />
+    <Aux>
+      {/* Since 'show' is a Boolean property, you don't need to assign a value and say 'show={true}' if it's true.
+      You can just say 'show'. Just by adding it, it will be set to 'true'. */}
+      <Backdrop show={props.open} clicked={props.closed} />
+      <div className={attachedClasses.join(' ')}>
+        <div className={classes.Logo}>
+          <Logo />
+        </div>
+        <nav>
+          <NavigationItems />
+        </nav>
       </div>
-      <nav>
-        <NavigationItems />
-      </nav>
-    </div>
+    </Aux>
   );
 };
 
