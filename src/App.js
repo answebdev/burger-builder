35921ce82f60a154bce414edaa05060cdeb1f857
ANS <36783010+answebdev@users.noreply.github.com>
2021-01-05 10:00:56 -0800

    added Orders Page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import Checkout from './containers/Checkout/Checkout';
+import Orders from './containers/Orders/Orders';
 
 class App extends Component {
   // For testing - to see if removing Axios interceptors in 'withErrorHandler' works:
@@ -23,6 +24,7 @@ class App extends Component {
           <Switch>
             <Layout>
               <Route path='/checkout' component={Checkout} />
+              <Route path='/orders' component={Orders} />
               <Route path='/' exact component={BurgerBuilder} />
             </Layout>
           </Switch>
