4f5a342bee208f8bb0fe2ce5c37da948f8150170
ANS <36783010+answebdev@users.noreply.github.com>
2021-01-06 10:12:18 -0800

    created dynamic input component

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Button from '../../../components/UI/Button/Button';
 import Spinner from '../../../components/UI/Spinner/Spinner';
+import Input from '../../../components/UI/Input/Input';
 import classes from './ContactData.module.css';
 import axios from '../../../axios-orders';
 
@@ -56,26 +57,26 @@ class ContactData extends Component {
   render() {
     let form = (
       <form>
-        <input
-          className={classes.Input}
+        <Input
+          inputtype='input'
           type='text'
           name='name'
           placeholder='Your Name'
         />
-        <input
-          className={classes.Input}
+        <Input
+          inputtype='input'
           type='email'
           name='email'
           placeholder='Your Email'
         />
-        <input
-          className={classes.Input}
+        <Input
+          inputtype='input'
           type='text'
           name='street'
           placeholder='Street'
         />
-        <input
-          className={classes.Input}
+        <Input
+          inputtype='input'
           type='text'
           name='postal'
           placeholder='Zip Code'
