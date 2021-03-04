3b7d37ae60cd769ba38f8bf7fefc84b7ce4f81ee
ANS <36783010+answebdev@users.noreply.github.com>
2021-03-04 11:41:11 -0800

    add logout functionality

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -22,6 +22,21 @@ export const authFail = (error) => {
   };
 };
 
+export const logout = () => {
+  return {
+    type: actionTypes.AUTH_LOGOUT,
+  };
+};
+
+export const checkAuthTimeout = (expirationTime) => {
+  return (dispatch) => {
+    setTimeout(() => {
+      dispatch(logout());
+      // SetTimeout expects to get milliseconds, so multiply by 1000 so we ge seconds, instead of milliseconds.
+    }, expirationTime * 1000);
+  };
+};
+
 export const auth = (email, password, isSignup) => {
   return (dispatch) => {
     dispatch(authStart());
@@ -41,6 +56,7 @@ export const auth = (email, password, isSignup) => {
       .then((response) => {
         console.log(response);
         dispatch(authSuccess(response.data.idToken, response.data.localId));
+        dispatch(checkAuthTimeout(response.data.expiresIn));
       })
       .catch((err) => {
         // console.log(err);
