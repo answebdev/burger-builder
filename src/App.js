36b305fc9004ff98c27a1d4a48e4a166586c1530
ANS <36783010+answebdev@users.noreply.github.com>
2021-03-11 11:07:36 -0800

    add navigation guards

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Route, Switch, withRouter } from 'react-router-dom';
+import { Route, Switch, withRouter, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
@@ -25,23 +25,41 @@ class App extends Component {
   // }
 
   render() {
+    // Routes for unauthenticated users:
+    let routes = (
+      <Switch>
+        <Route path='/auth' component={Auth} />
+        <Route path='/' exact component={BurgerBuilder} />
+        <Redirect to='/' />
+      </Switch>
+    );
+
+    // Routes for authenticated users:
+    if (this.props.isAuthenticated) {
+      routes = (
+        <Switch>
+          <Route path='/checkout' component={Checkout} />
+          <Route path='/orders' component={Orders} />
+          <Route path='/logout' component={Logout} />
+          <Route path='/' exact component={BurgerBuilder} />
+          <Redirect to='/' />
+        </Switch>
+      );
+    }
+
     return (
-      <BrowserRouter>
-        <div>
-          <Switch>
-            <Layout>
-              <Route path='/checkout' component={Checkout} />
-              <Route path='/orders' component={Orders} />
-              <Route path='/auth' component={Auth} />
-              <Route path='/logout' component={Logout} />
-              <Route path='/' exact component={BurgerBuilder} />
-            </Layout>
-          </Switch>
-        </div>
-      </BrowserRouter>
+      <div>
+        <Layout>{routes}</Layout>
+      </div>
     );
   }
 }
+
+const mapStateToProps = (state) => {
+  return {
+    isAuthenticated: state.auth.token !== null,
+  };
+};
 
 const mapDispatchToProps = (dispatch) => {
   return {
@@ -49,4 +67,4 @@ const mapDispatchToProps = (dispatch) => {
   };
 };
 
-export default withRouter(connect(null, mapDispatchToProps)(App));
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(App));
