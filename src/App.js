aca1ab9097e67d02bf4badca23b4f462037ca852
ANS <36783010+answebdev@users.noreply.github.com>
2021-03-11 10:31:02 -0800

    add local storage to persist auth state

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch, withRouter } from 'react-router-dom';
+import { connect } from 'react-redux';
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import Checkout from './containers/Checkout/Checkout';
 import Orders from './containers/Orders/Orders';
 import Auth from './containers/Auth/Auth';
 import Logout from './containers/Auth/Logout/Logout';
+import * as actions from './store/actions/index';
 
 class App extends Component {
+  componendDidMount() {
+    this.props.onTryAutoSignup();
+  }
   // For testing - to see if removing Axios interceptors in 'withErrorHandler' works:
   // state = {
   //   show: true,
@@ -38,4 +43,10 @@ class App extends Component {
   }
 }
 
-export default App;
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onTryAutoSignup: () => dispatch(actions.authCheckState()),
+  };
+};
+
+export default withRouter(connect(null, mapDispatchToProps)(App));
